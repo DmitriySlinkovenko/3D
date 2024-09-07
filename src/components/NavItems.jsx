cb1dba2841040ca88b9b9a5c1a52a5158bbb8db9
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-09-07 17:07:58 -0400

    NavBar complete

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { navLinks } from "../constants/index.js";
 
 const NavItems = () => {
   return (
     <ul className="nav-ul">
-      {["Home", "About", "Projects", "Contact"].map((item, index) => {
-        <li key={index} className="nav-li">
-          <a href="" className="nav-li_a">
-            {item}
+      {navLinks.map((item) => (
+        <li key={item.id} className="nav-li">
+          <a href={item.href} className="nav-li_a">
+            {item.name}
           </a>
-        </li>;
-      })}
+        </li>
+      ))}
     </ul>
   );
 };

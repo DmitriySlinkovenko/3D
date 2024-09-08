a9ce75a97ca8c52b3fe53c05a36c8d27d7f0dfeb
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-09-08 15:28:01 -0400

    Grid section 1,2,3 complete

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Hi, I'm Dmitriy</p>
+              <p className="grid-subtext">
+                Having finished a Software Engineering bootcamp, I have honed ny
+                skills in frontend and backend development, .
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:w-[276px] h-fit object-contain mx-auto"
+            />
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in JavaScript with a focus on React and Next.js
+                ecosystems
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 40,
+                    lng: -100,
+                    text: "I'm here!",
+                    color: "white",
+                    size: 9999,
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I work remotely across most tyimezones.
+              </p>
+              <p className="grid-subtext">
+                I'm based in New York, with remote work available.
+              </p>
+              <Button
+                name={"Contact Me"}
+                isBeam
+                containerClass="w-full mt-10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;

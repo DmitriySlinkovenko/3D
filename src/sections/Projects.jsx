0681a2436bfd9405ef627907c54ce23625d941ed
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-09-08 23:52:44 -0400

    Contact section styled

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -99,7 +99,7 @@ const Projects = () => {
             <directionalLight position={[10, 10, 5]} />
             <Center>
               <Suspense fallback={<CanvasLoader />}>
-                <group scale={0.5} position={[0, 0, 0]} rotation={[0, -0.1, 0]}>
+                <group scale={0.6} position={[0, 0, 0]} rotation={[0, -0.1, 0]}>
                   <DemoComputer texture={currentProject.texture} />
                 </group>
               </Suspense>
